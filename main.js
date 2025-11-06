3c78d3710ba4ab0eb9c41fe3a96d8beacadfd24f
Giovanni Ambriz <giovanni.ambriz@hotmail.com>
2025-11-06 21:42:40 +1300

    Add function to handle income input and result

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
-// Create function to calulate tax based on income
-// Create condition to assign tax % to income
-// Result
-
 function calculateTax(income) {
   let tax = 0
 
   if (income <= 15600) {
-    tax = income * 0.105
+    tax = income * 0.105;
   } else if (income <= 53500) {
-    tax = income * 0.175
+    tax = income * 0.175;
   } else if (income <= 78100) {
-    tax = income * 0.30
+    tax = income * 0.30;
   } else if (income <= 180000) {
-    tax = income * 0.33
+    tax = income * 0.33;
   } else {
     tax = income * 0.39
   }
 
-  return tax
+  return tax;
 }
 
-console.log(calculateTax(10000))
+document.getElementById('calculate').addEventListener('click', () => {
+  const incomeInput = document.getElementById('income').value;
+  const income = parseFloat(incomeInput);
+
+  const tax = calculateTax(income);
+  document.getElementById('result').textContent = `$${tax.toFixed(2)}`;
+})
